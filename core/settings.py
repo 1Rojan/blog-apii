9f4cd995da75b9571a7dba34c1aa84a6d99dc1fc
1Rojan <rznsnr11@gmail.com>
2021-07-05 12:04:42 +0545

    postgres db setup

--- core/settings.py
+++ core/settings.py
@@ -73,12 +73,6 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
@@ -123,3 +117,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+from .local_settings import DATABASES
