5d82bb4b271b487ed2f99d7d3a183f1dce9f9c18
1Rojan <rznsnr11@gmail.com>
2021-07-05 22:25:14 +0545

    configurations

--- api/models.py
+++ api/models.py
@@ -1,16 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Post(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length=100, blank=True, default="")
     body = models.TextField(blank=True, default="")
-    owner = models.ForeignKey(
-        "auth.User", related_name="posts", on_delete=models.CASCADE
-    )
+    owner = models.ForeignKey(User, related_name="posts", on_delete=models.CASCADE)
 
     def __str__(self):
-      return self.title
+        return self.title
 
     class Meta:
         ordering = ["created"]
@@ -19,9 +18,7 @@ class Post(models.Model):
 class Comment(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     body = models.TextField(blank=False)
-    owner = models.ForeignKey(
-        "auth.User", related_name="comments", on_delete=models.CASCADE
-    )
+    owner = models.ForeignKey(User, related_name="comments", on_delete=models.CASCADE)
     post = models.ForeignKey("Post", related_name="comments", on_delete=models.CASCADE)
 
     class Meta:
@@ -30,9 +27,7 @@ class Comment(models.Model):
 
 class Category(models.Model):
     name = models.CharField(max_length=100, blank=False, default="")
-    owner = models.ForeignKey(
-        "auth.User", related_name="categories", on_delete=models.CASCADE
-    )
+    owner = models.ForeignKey(User, related_name="categories", on_delete=models.CASCADE)
     posts = models.ManyToManyField("Post", related_name="categories", blank=True)
 
     def __str__(self):
