8a47257f682ea54af905b784350090b9fcf8c0d8
1Rojan <rznsnr11@gmail.com>
2021-07-05 15:59:11 +0545

    index template, routing

--- api/urls.py
+++ api/urls.py
@@ -1,4 +1,26 @@
 from django.urls import path
+from .views import (
+    Home,
+    UserRegister,
+    PostCreate,
+    PostList,
+    UserList,
+    UserDetail,
+    PostDetail,
+)
 
 urlpatterns = [
+    path("", Home.as_view()),
+    path("users/register/", UserRegister.as_view()),
+    path(
+        "users/",
+        UserList.as_view(),
+    ),
+    path(
+        "users/<pk>/",
+        UserDetail.as_view(),
+    ),
+    path("posts/", PostList.as_view()),
+    path("posts/create/", PostCreate.as_view()),
+    path("posts/<pk>", PostDetail.as_view()),
 ]
--- api/views.py
+++ api/views.py
@@ -1,2 +1,57 @@
+from django.contrib.auth.models import User
+from django.db import models
 from django.shortcuts import render
+from django.views.generic import TemplateView
+from rest_framework import serializers
+from rest_framework.generics import (
+    CreateAPIView,
+    ListAPIView,
+    RetrieveAPIView,
+    ListAPIView,
+    RetrieveUpdateDestroyAPIView,
+)
+from rest_framework.permissions import IsAdminUser, IsAuthenticated
+from .serializers import UserSerializer, PostSerializer
+from .models import Post
 
+
+class Home(TemplateView):
+    template_name = "home.html"
+
+
+class UserRegister(CreateAPIView):
+    serializer_class = UserSerializer
+
+
+class UserList(ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class PostList(ListAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+
+class PostCreate(CreateAPIView):
+    serializer_class = PostSerializer
+    permission_classes = [
+        IsAuthenticated,
+    ]
+
+    def perform_create(self, serializer):
+        if self.request.user:
+            serializer.save(owner=self.request.user)
+
+
+class PostDetail(RetrieveUpdateDestroyAPIView):
+    serializer_class = PostSerializer
+    permission_classes = [
+        IsAdminUser,
+        IsAuthenticated,
+    ]
